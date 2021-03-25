18bcf153203aede0b94d3b8bdcd2c269bfee164a
andrewmillen <andrewmillen1@gmail.com>
2021-03-25 09:41:06 -0500

    Configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        wine: "#66191D",
+        marigold: "#E2A361",
+        olive: "#6B6B54",
+        linen: "DBCDC7",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
